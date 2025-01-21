6c31ef358549ee9a99a8a0edfe62c77f92af9caf
ROHITH <rohith171801@gmail.com>
2025-01-21 17:12:40 +0530

    isPrime  modified

--- Tech round/LAB1.java
+++ Tech round/LAB1.java
@@ -66,7 +66,10 @@ public class LAB1 {
     }
 
     public static boolean isPrime(int n) {
-        for (int i = 2; i < Math.sqrt(n); i++) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
             }
