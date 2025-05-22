064d352a55e32e81d445cd8d8d3218896acd58c4
Rohith M <rohith171801@gmail.com>
2025-05-22 22:18:12 +0530

    Security And Athentication

--- PythonRebornIn60/FastAPI/SecurityAndAuthentication/heroes.py
+++ PythonRebornIn60/FastAPI/SecurityAndAuthentication/heroes.py
@@ -113,7 +113,7 @@ async def get_current_user(
         username = payload.get("sub")
         if username is None:
             raise credentials_exception
-    except:
+    except InvalidTokenError:
         raise credentials_exception
     user = db.exec(select(User).where(User.username == username)).first()
     if not user:
